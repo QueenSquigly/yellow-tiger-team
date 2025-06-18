41fd402d7f20afe6e77e020ae600a2751d57a8d2
QueenSquigly <queensquigly@gmail.com>
2025-06-18 11:15:54 -0700

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,16 @@
+name = "taurean"
+nickname = "taur"
+#age var int
+age = 14
+#python boolean
+used_python = False
+
+#hobbies list
+hobbies = ["playing games", "watching anime"]
+#libary
+fav_things ={"food":"pasta", "games":"wuwa"}
+#float
+
+print(name)
+print(nickname)
+print(age)
