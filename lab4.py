9ce49964179a257681cce0945a90f79ebe2c61fa
QueenSquigly <queensquigly@gmail.com>
2025-07-07 13:27:40 -0700

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,33 @@
+#cheking = "yes"
+
+#while cheking == "yes":
+    #print("what is your age")
+    #use
+    #if int(user_input) >= 18:
+            #print("yes you can vote")
+    #else:
+          #print("you cant vote")
+    #print("would you like to check another age?")
+    #user_input2 = input()
+    #checking = user_input2
+
+#list1 = [2, -1, 0, 6, -4]
+
+#for x in list1:
+    #if x > 0:
+        #print("value is positive")
+#else:  x < 0
+#print("value is negitive")
+#elif: x = 0
+inventory = ["tnt", "redstone", "grass", "iron", "diamond"]
+for i in inventory:
+    if i == "tnt": 
+        print ("why do you have this")
+    elif i == "grass":
+        print("grass is cool")
+    elif i == "redstone":
+        print("trap?") 
+    elif i == "iron":
+        print("going for the blue ore!")
+    elif i == "diamond":
+        print("cheater")
