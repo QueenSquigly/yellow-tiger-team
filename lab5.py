2ffa6c6db432c278e21bebc476d540a4fa6008a7
QueenSquigly <queensquigly@gmail.com>
2025-07-07 13:13:50 -0700

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,46 @@
+#hii
+def cat_greeting(word):
+        print(f'cat says {word}')
+        print('cat says nothing')
+
+cat_greeting("hi")
+
+#step 2
+#def generate_superhero_power():
+        #name = "sigma"
+        #power = "speed"
+        #print(f"{name}'s power is {power}")
+
+#generate_superhero_power()
+
+#step 3
+#def generate_superhero_power():
+        #power = "flying"
+        #return power
+
+#print(generate_superhero_power())
+
+#step 4
+#def generate_superhero_power(user_name, super_power):
+        #message = user_name + " has the power of " + super_power + "!"
+        #return message
+#print(generate_superhero_power("herta", "clones"))
+
+
+#step 5
+def cat_greeting_loop():
+    greetings = ["meow", "mo", "Mwee", "pur", "puur"]
+    for i in greetings:
+        
+        print(f'The cat says {i}')
+    
+    
+cat_greeting_loop()
+
+#step 6
+#def generate_multi_powers(powers):
+    #for i in powers:
+       # print(i)
+
+#powers_test = ["super speed", "super strength", "flying"]
+#generate_multi_powers(powers_test)
